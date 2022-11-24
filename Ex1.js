7b15aa32547443c1509ad8ad1b48b1c8570a60e4
CodeNicee <94329832+CodeNicee@users.noreply.github.com>
2022-11-24 15:29:26 +0700

    a

---
+++ Ex1.js
@@ -0,0 +1,95 @@
+let Employee = [
+    {
+        "ID" : "10001",
+        "Name" : "นางสาวชนิสา ไกรรอด",
+        "Department" : "ไอที",
+        "Position" : "System Engineer",
+        "Salary" : "42000",
+        "Bonus" : "S"
+    },
+    {
+        "ID" : "10002",
+        "Name" : "นายวศพล สิริอนันต์",
+        "Department" : "ไอที",
+        "Position" : "System Engineer",
+        "Salary" : "13000",
+        "Bonus" : "A"
+    },
+    {
+        "ID" : "10003",
+        "Name" : "นางจิราพร อิทรประเสริฐ",
+        "Department" : "ฝ่ายบุคคล",
+        "Position" : "HR",
+        "Salary" : "9000",
+        "Bonus" : "C"
+    },
+    {
+        "ID" : "10004",
+        "Name" : "นางสาวพิมพา กาญจนมาศ",
+        "Department" : "ฝ่ายบุคคล",
+        "Position" : "HR",
+        "Salary" : "21000",
+        "Bonus" : "B"
+    },
+    {
+        "ID" : "10005",
+        "Name" : "นายจิรโชติ วัฒนโกศล",
+        "Department" : "ฝ่ายบุคคล",
+        "Position" : "HR",
+        "Salary" : "22000",
+        "Bonus" : "A"
+    },
+    {
+        "ID" : "10006",
+        "Name" : "นายธัชพล ขจรเกียรติสกุล",
+        "Department" : "การเงิน",
+        "Position" : "Account",
+        "Salary" : "12000",
+        "Bonus" : "S"
+    },
+    {
+        "ID" : "10007",
+        "Name" : "นายะนวิทย์ สุวรรณโชติ",
+        "Department" : "การเงิน",
+        "Position" : "Account",
+        "Salary" : "14000",
+        "Bonus" : "A"
+    },
+    {
+        "ID" : "10008",
+        "Name" : "นางพัชรียา สำราญทรัพย์",
+        "Department" : "การเงิน",
+        "Position" : "Account",
+        "Salary" : "50000",
+        "Bonus" : "C"
+    },
+    {
+        "ID" : "10009",
+        "Name" : "นางสาวขวัญชนก ธาดาวรวงศ์",
+        "Department" : "ไอที",
+        "Position" : "System Engineer",
+        "Salary" : "16000",
+        "Bonus" : "A"
+    }
+    
+]
+
+let Bonus = [
+    {
+        "level" : "S",
+        "BonusRate" : "1.4"
+    },
+    {
+        "level" : "A",
+        "BonusRate" : "1.2"
+    },
+    {
+        "level" : "B",
+        "BonusRate" : "1.0"
+    },
+    {
+        "level" : "C",
+        "BonusRate" : "0.8"
+    }
+]
+
